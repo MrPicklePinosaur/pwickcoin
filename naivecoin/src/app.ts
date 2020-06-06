6d2b562f8d755345968ccf0d36c0166b46b40834
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-06-05 22:58:20 -0400

    restarted oof

--- naivecoin/src/app.ts
+++
@@ -1,4 +0,0 @@
-import {Block} from './block.ts'
-
-const b = new Block(0,'hash','prevhash',12,'data');
-console.log('ready');
---
+++ naivecoin/src/index.ts
@@ -0,0 +1,4 @@
+import sha256 from 'crypto-js/sha256';
+
+console.log(sha256("1").toString());
+
