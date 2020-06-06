8586a38efaf65daf63984663f436c4bc1b8879dc
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-06-05 21:46:01 -0400

    init in app

---
+++ app/src/components/Landing.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="panel-landing" id="section-1">
+    <h1 class="landing-heading">Hello, Blockstack!</h1>
+    <p class="lead">
+      <a
+        href="#"
+        class="btn btn-primary btn-lg"
+        id="signin-button"
+        @click.prevent="signIn"
+      >
+        Sign In with Blockstack
+      </a>
+    </p>
+  </div>
+</template>
+
+<script>
+import { userSession } from '../userSession'
+import { showBlockstackConnect } from '@blockstack/connect'
+export default {
+  name: 'signin',
+  methods: {
+    signIn () {
+      const authOptions = {
+        appDetails: {
+          name: 'Blockstack App',
+          icon: window.location.origin + '/favicon.ico'
+        },
+        userSession,
+        finished: ({ userSession }) => {
+          window.location = window.location.origin
+        }
+      }
+      showBlockstackConnect(authOptions)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
