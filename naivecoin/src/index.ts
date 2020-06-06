19ac495a299b6c909067cbd3b73b405ce11da065
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-06-06 14:21:41 -0400

    im big rotard

--- naivecoin/src/index.ts
+++ naivecoin/src/index.ts
@@ -1,9 +1,23 @@
 import { Blockchain } from './Blockchain'
+import express from 'express'
+
 
 const bc = new Blockchain();
 
-bc.generateBlock('1');
-bc.generateBlock('2');
+const app = express();
+
+app.get('/blockchain', (req, res) => {
+    res.send(JSON.stringify(bc));
+});
+app.post('/newblock', (req, res) => {
+    //generate a new block
+    
+});
+
+app.listen(5000);
+
+/*
 console.log(bc.blockchain);
 
 console.log(bc.validateBlockChain(bc.blockchain));
+*/
