6e8d19baf7c1e5293baf7657e0299c2c10fe57a1
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-06-06 22:11:49 -0400

    wakarimashita

--- naivecoin/src/Transaction.ts
+++ naivecoin/src/Transaction.ts
@@ -4,43 +4,70 @@ import * as ecdsa from 'elliptic'
 export const ec = new ecdsa.ec('secp256k1');
 
 export type TransIn = {
-    id: string,
-    index: number,
-    signature: string;
+
+    outId: string, //owner of the money
+    outIndex: number, //reference to the TransOut that the money came from
+    signature: string
 }
 
 export type TransOut = {
     address: string, //target of transaction
-    amount: number
+    amount: number 
+}
+
+export type UnspentTransOut = { //leftover money from transaction, made as a transaction to self
+    readonly id: string,  
+    readonly index: number, //value to reference this blob of money
+    readonly address: string, //owner of the money
+    readonly amount: number
 }
 
 export class Transaction {
 
-    public id: string = '';
-    public transIn: TransIn[] = [];
-    public transOut: TransOut[] = [];
+    public id: string = ''; //a random string (should be 256 bits)
+    public hash: string = '';
+    public transInList: TransIn[] = [];
+    public transOutList: TransOut[] = [];
 
     constructor() {
-
+        //generate a random id for this
     }
 
-    calculateTransactionId(): string {
+    //only use when transaction object is full
+    calculateTransactionHash(): string {
 
-        const transInString = this.transIn
-            .map(({id, index}: TransIn) => id+index )
+        const transInString = this.transInList
+            .map(({outId, outIndex}: TransIn) => outId+outIndex )
             .reduce((acc, cur) => acc+cur, '');
 
-        const transOutString = this.transOut
+        const transOutString = this.transOutList
             .map(({address, amount}: TransOut) => address+amount )
             .reduce((acc, cur) => acc+cur, '');
 
-        return sha256(transInString+transOutString).toString();
+        return sha256(this.id+transInString+transOutString).toString();
 
     }
 
-    signTransaction(transInIndex: number, privateKey: string): string {
+    //not finished
+    static signTransaction({id, transInList}: Transaction, transInIndex: number, privateKey: string): string {
 
-        return '';
+        const transIn = transInList[transInIndex]; //the specific transaction we are signing
+
+        const key = ec.keyFromPrivate(privateKey,'hex');
+        const signature = Transaction.toHexString(key.sign(id).toDER()); 
+
+        return signature;
+    }
+
+    //from https://github.com/lhartikk/naivecoin/blob/chapter3/src/transaction.ts
+    static toHexString(byteArray: string) {
+        return Array.from(byteArray, (byte: any) => {
+            return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+        }).join('');
+    };
+
+    static generateKeyPair() {
+        return ec.genKeyPair();
     }
 
 
