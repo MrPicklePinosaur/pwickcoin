01b090c1434975f0ac937862e1f05ce363eaf309
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-06-07 09:29:26 -0400

    Routes are set up correctly

--- frontend/web/src/views/Login.vue
+++ frontend/web/src/views/Login.vue
@@ -1,11 +1,33 @@
 <template>
-    <div>
+    <div class = "login-main">
         <p>Login</p>
+        <form @submit.prevent="onSubmit">
+            <input type="text" v-model = "user"  placeholder="User">
+            <input type="text" v-model = "password"  placeholder="Password">
+            <input type="submit" value="Submit" class="btn">
+        </form>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Login"
+    name: "Login",
+    methods: {
+        onSubmit(){
+            const lmao = {
+                user:this.user,
+                password:this.password
+            }
+            this.$emit('login',lmao);
+        }
+    }
 }
+
 </script>
+
+<style scoped>
+    .login-main {
+        background-color: aquamarine;
+
+    }
+</style>
